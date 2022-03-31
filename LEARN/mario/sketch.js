28dd299507b74b7938848b66b70fffc78b407baf
chrissavov <chrissavov@hotmail.com>
2022-03-31 04:19:50 -0400

    improve

--- LEARN/mario/sketch.js
+++ LEARN/mario/sketch.js
@@ -48,4 +48,37 @@ function setup() {
   }
 }
 
-function draw() {}
+function draw() {for (let col = 0; col < img.width; col += 10) {
+    for (let row = 0; row < img.height; row += 10) {
+      let xPos = col;
+      let yPos = row;
+
+      let c = img.get(xPos, yPos);
+      let type = random(10);
+
+      if (type > 1) {
+        fill(color(c));
+        rect(xPos, yPos, 10, 5);
+      } else {
+        push();
+        translate(xPos, yPos);
+        rotate(radians(random(360)));
+        noFill();
+        strokeWeight(random(2));
+        stroke(color("#B026FF"));
+        // stroke(color(c));
+        curve(
+          xPos,
+          yPos,
+          sin(yPos) * random(60),
+          cos(xPos) * random(40),
+          0,
+          0,
+          cos(yPos) * sin(xPos) * random(140),
+          cos(xPos) * sin(xPos) * random(50)
+        );
+        pop();
+      }
+    }
+  }
+}
